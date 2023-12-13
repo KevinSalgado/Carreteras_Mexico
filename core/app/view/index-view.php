769f0409daef0c453bb9fd9f17019e8b7938414d
holly huerta <holly.cg@outlook.com>
2023-12-13 01:12:58 -0600

    Carrusel de imagenes

--- core/app/view/index-view.php
+++ core/app/view/index-view.php
@@ -24,8 +24,8 @@
                 <td><?php echo $row->Categoria; ?></td>
                 <td><?php echo $row->Carretera_Anterior; ?></td>
                 <td><?php echo $row->Carretera_Posterior; ?></td>
-                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Tramos </a> </td>
-                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Confluye </a> </td>
+                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Tramos </a> </td>
+                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Confluye </a> </td>
             </tr>
             <?php
                 }
@@ -62,8 +62,8 @@
                 <td><?php echo $row->Categoria; ?></td>
                 <td><?php echo $row->Carretera_Anterior; ?></td>
                 <td><?php echo $row->Carretera_Posterior; ?></td>
-                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Tramos </a> </td>
-                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Confluye </a> </td>
+                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Tramos </a> </td>
+                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Confluye </a> </td>
             </tr>
             <?php
                 }
@@ -99,8 +99,8 @@
                 <td><?php echo $row->Categoria; ?></td>
                 <td><?php echo $row->Carretera_Anterior; ?></td>
                 <td><?php echo $row->Carretera_Posterior; ?></td>
-                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Tramos </a> </td>
-                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Confluye </a> </td>
+                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Tramos </a> </td>
+                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Confluye </a> </td>
             </tr>
             <?php
                 }
@@ -136,8 +136,8 @@
                 <td><?php echo $row->Categoria; ?></td>
                 <td><?php echo $row->Carretera_Anterior; ?></td>
                 <td><?php echo $row->Carretera_Posterior; ?></td>
-                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Tramos </a> </td>
-                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Confluye </a> </td>
+                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Tramos </a> </td>
+                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Confluye </a> </td>
             </tr>
             <?php
                 }
@@ -173,8 +173,8 @@
                 <td><?php echo $row->Categoria; ?></td>
                 <td><?php echo $row->Carretera_Anterior; ?></td>
                 <td><?php echo $row->Carretera_Posterior; ?></td>
-                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Tramos </a> </td>
-                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-warning"><i class="fa fa-pencil"></i> Confluye </a> </td>
+                <td><a href="./?view=tramo&opt=tramos&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Tramos </a> </td>
+                <td><a href="./?view=confluye&opt=confluye&Carretera=<?php echo $row->Carretera;?>" class="btn btn-outline-gray btn-sm"><i class="fa fa-pencil"></i> Confluye </a> </td>
             </tr>
             <?php
                 }
@@ -191,6 +191,46 @@
 <?php
     $listaCarreteras = CarreterasData::getAll();
 ?>
+<div class="bd-example">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item">
+                        <img src="imagenes/carros1.jpg">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Carreteras México</h5>
+                            <p>Imagenes representativas de carreteras.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item active">
+                       <img src="imagenes/carros2.jpg">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Carreteras México</h5>
+                            <p>Imagenes representativas de carreteras.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="imagenes/carros3.jpg">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Carreteras México</h5>
+                            <p>Imagenes representativas de carreteras.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
 
 <p class="h1">Carreteras general</p>
 <div class="bd-example table-responsive">
